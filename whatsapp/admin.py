e0b9b755241453a92d1985209e47e49a99f5597d
arjunmadavoor <arjunmdrcalicut@gmail.com>
2023-04-01 18:59:57 +0530

    db added to admin

--- whatsapp/admin.py
+++ whatsapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import UserData
+
+class UserDataAdmin(admin.ModelAdmin):
+    list_display = ('mobile_number', 'user_status', 'created_date')
+    search_fields = ('mobile_number',)
+    list_filter = ('user_status',)
+
+admin.site.register(UserData, UserDataAdmin)
